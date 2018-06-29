3b56baa3c4d9e737f14bb6bdc6e16baec6e0610d
Melody8988 <melody.massard8988@gmail.com>
2018-06-29 13:19:19 -0500

    unit test

--- MarketTests/unique_segments_test.py
+++ MarketTests/unique_segments_test.py
@@ -3,6 +3,9 @@ from python_challenge import MarketSegment
 
 class TestUniqueSegmentAccounts(unittest.TestCase):
     def test_account(self):
+        print('hi')
         # Test if incoming account currently exists in the segment's array
-        self.isInstance(add_account(acc1, MarketSegment('Consumer Goods', [acc1, acc2])))
+        # self.isInstance(add_account(acc1, MarketSegment('Consumer Goods', [acc1, acc2])))
         
+    
+   
